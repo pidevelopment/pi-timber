19405aae07979141b5daaceac2bdf63ec64c3cd8
Kory Mathis <kory.mathis@gmail.com>
2015-08-31 17:02:03 -0700

    added version to style src

--- functions.php
+++ functions.php
@@ -29,11 +29,9 @@ class StarterSite extends TimberSite {
 	}
 
 	function add_to_context( $context ) {
-		$context['foo'] = 'bar';
-		$context['stuff'] = 'I am a value set in your functions.php file';
-		$context['notes'] = 'These values are available everytime you call Timber::get_context();';
 		$context['menu'] = new TimberMenu();
 		$context['site'] = $this;
+		$context['version'] = wp_get_theme()->get('Version');
 		return $context;
 	}
 
@@ -47,8 +45,3 @@ class StarterSite extends TimberSite {
 }
 
 new StarterSite();
-
-function myfoo( $text ) {
-	$text .= ' bar!';
-	return $text;
-}
